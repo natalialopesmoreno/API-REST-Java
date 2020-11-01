623a4cdee1dbeb19abc13ac72d97fe1f5c242599
Natália L. Moreno <63927404+natalialopesmoreno@users.noreply.github.com>
2020-11-01 20:18:19 -0300

    CRUD inserindo Update e Delete

--- API-REST-JAVA/src/main/java/com/br/api/rest/java/controller/UsuarioController.java
+++ API-REST-JAVA/src/main/java/com/br/api/rest/java/controller/UsuarioController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,11 +54,36 @@ public class UsuarioController
 			return ResponseEntity.ok().body(repository.findById(id).get());
 		}
 	
+		
+		
 		//UPDATE - CRUD
+		
+		
+		@PutMapping("/alterar/{id}")
+		public ResponseEntity<UsuarioModel> alterar(@PathVariable Long id, @RequestBody UsuarioModel usuario)
+		{
+			return repository.findById(id)
+					.map(contatoAchado -> {	contatoAchado.setNome(usuario.getNome());
+											contatoAchado.setLogin(usuario.getLogin());
+											contatoAchado.setSenha(usuario.getSenha());
+											UsuarioModel updated = repository.save(contatoAchado);
+											return ResponseEntity.ok().body(updated);
+											}).orElse(ResponseEntity.notFound().build());
+		}
+		
 		
 		
 		//DELETE - CRUD
 		
+		@DeleteMapping("/delete/{id}")
+		public ResponseEntity deletar(@PathVariable Long id)
+		{
+			return repository.findById(id).map(contatoDeletar->
+			{
+				repository.deleteById(id);
+				 return ResponseEntity.ok().build();
+			}).orElse(ResponseEntity.notFound().build());
+		}
 		
 		
 }
