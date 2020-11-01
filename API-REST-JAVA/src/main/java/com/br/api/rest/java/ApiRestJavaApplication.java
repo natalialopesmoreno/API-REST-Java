6670fb3080821ab041806f7170869449e04115c9
Natália L. Moreno <63927404+natalialopesmoreno@users.noreply.github.com>
2020-11-01 19:56:04 -0300

    Criação das camadas do CRUD - Creat - Read

---
+++ API-REST-JAVA/src/main/java/com/br/api/rest/java/ApiRestJavaApplication.java
@@ -0,0 +1,13 @@
+package com.br.api.rest.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestJavaApplication.class, args);
+	}
+
+}
